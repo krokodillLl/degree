c05eb7f120e8f5a5c459630890666a8cc24e9120
krokodillLl <53301367+krokodillLl@users.noreply.github.com>
2021-05-23 00:44:32 +0300

    refactor

--- src/main/java/com/krokodillLl/degree/Application.java
+++ src/main/java/com/krokodillLl/degree/Application.java
@@ -2,8 +2,8 @@ package com.krokodillLl.degree;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
